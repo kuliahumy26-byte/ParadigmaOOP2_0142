a3c7b5eebeb5be7ff9f1440fe5613a6f71632abc
kuliahumy26-byte <kuliahumy26@gmail.com>
2026-05-19 18:11:22 +0700

    membuat class base dan derived

--- noOverriding.cpp
+++ noOverriding.cpp
@@ -1,3 +1,15 @@
 #include <iostream>
 using namespace std;
 
+class baseClass {
+public:
+    virtual void perkenalan() final{
+        cout << "Hallo saya Function dari base class";
+    }
+};
+class derivedClass : public baseClass {
+public:
+    void perkenalan1() {
+        cout << "Hallo saya Function dari derived class";
+    }
+};
