95f171a4b94aa4faa82865d85751676daa496d1a
kuliahumy26-byte <kuliahumy26@gmail.com>
2026-05-19 18:10:35 +0700

    membuat int main

--- Polimorpi.cpp
+++ Polimorpi.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 class seseorang {
     public:
-    virtual void pesan() = 0;
+    //virtual void pesan() = 0;
      virtual void pesan(){
      cout<<"Pesan dari seseorang"<<endl;
       }
@@ -24,3 +24,16 @@ public:
 
 };
 
+int main() {
+    seseorang* obyek;
+    joko a;
+    lia b;
+
+    obyek = &a;
+    obyek->pesan();
+    obyek = &b;
+    obyek->pesan();
+    //a.seseorang::pesan();
+    //b.seseorang::pesan();
+    return 0;
+}
